cf4b57693e417272229c201791a329fa7f9bb2b1
youtlane <youtlanehanane@gmail.com>
2024-05-05 21:35:03 +0200

    feat: highliting the active page

--- kasa/src/components/header/Header.jsx
+++ kasa/src/components/header/Header.jsx
@@ -1,21 +1,42 @@
 // Header.js
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.jpg';
 import './HeaderStyle.scss';
 
 export default function Header() {
+    const[currentRoute, setCurrentRoute] = useState()
+    useEffect(()=>{
+        const path = window.location.pathname;
+        setCurrentRoute(path);
+    },[]);
+    
     return (
         <header className="header">
             <div className="header_logo">
                 <Link to="/">
+                    
                     <img className="logo" src={logo} alt="Logo" />
                 </Link>
             </div>
             <nav className="header_nav">    
-                <Link to="/" className="header_nav_link-home">Accueil</Link>
-                <Link to="/about" className="header_nav_link-about">A Propos</Link>
+                <Link to="/" 
+                onClick={() => setCurrentRoute("/")}
+                className={
+                    currentRoute==="/"
+                    ? "header_nav_link-home-active"
+                    : "header_nav_link-home"
+                    }>
+                    Accueil</Link>
+                <Link to="/about"
+                onClick={() => setCurrentRoute("about")}
+                className={
+                    currentRoute==="about"
+                    ? "header_nav_link-home-active"
+                    : "header_nav_link-home"
+                    }>
+                A Propos</Link>
             </nav>
         </header>
     );
