74dbcb44c237ea913bcf923a067331c326499e46
youtlane <youtlanehanane@gmail.com>
2024-05-06 17:10:44 +0200

    feat: creating the banner component

---
+++ kasa/src/components/Banner/Banner.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './BannerStyle.scss';
+
+
+
+export default function Banner({ type, title, backgroundImage }) {
+    const bannerStyle = {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${backgroundImage})`
+       
+        
+    };
+
+    return (
+        <div className="banner" style={bannerStyle}>
+            {type === 'home' && (
+                <h1>{title}</h1>
+            )}
+            {type === 'about' && title && (
+                <h1>{title}</h1>
+            )}
+        </div>
+    );
+}
--- kasa/src/pages/home/Home.jsx
+++ kasa/src/pages/home/Home.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Banner from '../../components/Banner/Banner';
+
+import homeBackgroundImage from '../../assets/homeBackground.jpg';
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  );
+    return (
+        <main>
+            <Banner type="home" title="Chez vous, partout et ailleurs" backgroundImage={homeBackgroundImage} />
+            {/* Autres contenus de la page d'accueil */}
+        </main>
+    );
 }
