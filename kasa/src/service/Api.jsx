790535458a6be47d64eae921ac54751afda3d8e9
youtlane <youtlanehanane@gmail.com>
2024-05-17 16:36:48 +0200

    fix: importing data from the json file

--- kasa/src/service/Api.jsx
+++ kasa/src/service/Api.jsx
@@ -1,21 +1,18 @@
 
-
-// Importer le fichier db.json
 import data from '../data/db.json';
 
 // Fonction pour récupérer toutes les propriétés
 export const getAllProperties = () => {
-  return data.properties; // Retourne toutes les propriétés du fichier db.json
+  return data.properties; 
 };
 
 // Fonction pour récupérer une propriété par ID
 export const getPropertyById = (id) => {
-  return data.properties.find(property => property.id === id); // Retourne la propriété correspondant à l'ID donné
+  return data.properties.find(property => property.id === id); 
 };
 
 // Fonction pour récupérer toutes les pages "About"
 export const getAboutPageData = () => {
-  return data.aboutPage; // Retourne toutes les pages "About" du fichier db.json
+  return data.aboutPage; 
 };
 
-// Ajoutez d'autres fonctions API ici au besoin
